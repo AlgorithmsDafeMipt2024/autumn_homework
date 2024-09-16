79c77b3394e9125e01107ea819d2fb9c59a12c4a
Ivan Khokhlov <lostpointmeister@gmail.com>
2024-09-16 08:11:52 +0000

    tst

--- lib/src/util.cpp
+++ lib/src/util.cpp
@@ -1,1 +1,5 @@
 #include "util.hpp"
+
+#include <algorithm>
+
+int Max(const std::list<int>& l) { return std::max(l.begin(), l.end()); }
---
+++ lib/src/util.hpp
@@ -0,0 +1,3 @@
+#include <list>
+
+int Max(const std::list<int>& l);
