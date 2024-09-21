2f35a069539e389f0c1b04774ac4690fed4727af
Sukhanov George <sukhanov.ge@phystech.edu>
2024-09-21 11:38:46 +0000

    task_04 done

--- lib/src/graph.h
+++ lib/src/graph.h
@@ -1,7 +1,9 @@
 #pragma once
 
+#include <cmath>
 #include <list>
 #include <stdexcept>
+#include <vector>
 
 class Graph {
  private:
@@ -25,4 +27,51 @@ class Graph {
   void TopologySortStep(int current_vertice, bool visited_vertices[],
                         std::list<int>& list);
   std::list<int> TopologySort();
+};
+
+class Weighted_Graph {
+ private:
+  std::vector<std::vector<int>> table;
+  int vertices_number;
+
+ public:
+  Weighted_Graph(std::vector<std::vector<int>> table_) {
+    if (table_.empty())
+      throw std::logic_error(
+          "the graph was not created because the input table is empty");
+    for (int i = 0; i < table_.size(); i++)
+      if (table_[i].size() != table_.size())
+        throw std::logic_error(
+            "the graph was not created because the input table is incorrect");
+    table = table_;
+    vertices_number = table.size();
+  }
+
+  int MinDistance(std::vector<int> distances, bool is_shortest[]) {
+    int min_distance = 2 ^ 31 - 1, min_index;
+    for (int i = 0; i < vertices_number; i++)
+      if (!is_shortest[i] && distances[i] < min_distance)
+        min_distance = distances[i], min_index = i;
+    return min_index;
+  };
+  std::vector<int> Dijkstra_algo(int source) {
+    source -= 1;
+    std::vector<int> distances;
+    bool is_shortest[vertices_number];
+    for (int i = 0; i < vertices_number; i++) {
+      distances.push_back(std::pow(2, 31) - 1);
+      is_shortest[i] = false;
+    }
+    distances[source] = 0;
+    for (int i = 0; i < vertices_number - 1; i++) {
+      int min_distance = MinDistance(distances, is_shortest);
+      is_shortest[min_distance] = true;
+      for (int j = 0; j < vertices_number; j++)
+        if (!is_shortest[j] && table[min_distance][j] &&
+            distances[j] != (2 ^ 31 - 1) &&
+            distances[min_distance] + table[min_distance][j] < distances[j])
+          distances[j] = distances[min_distance] + table[min_distance][j];
+    }
+    return distances;
+  }
 };
--- task_04/src/test.cpp
+++ task_04/src/test.cpp
@@ -1,6 +1,40 @@
-
+#include <graph.h>
 #include <gtest/gtest.h>
 
-TEST(TopologySort, Simple) {
-  ASSERT_EQ(1, 1);  // Stack []
+TEST(TopologySort, Simple1) {
+  std::vector<std::vector<int>> table = {
+      {0, 4, 0, 0, 0, 0, 0, 8, 0},  {4, 0, 8, 0, 0, 0, 0, 11, 0},
+      {0, 8, 0, 7, 0, 4, 0, 0, 2},  {0, 0, 7, 0, 9, 14, 0, 0, 0},
+      {0, 0, 0, 9, 0, 10, 0, 0, 0}, {0, 0, 4, 14, 10, 0, 2, 0, 0},
+      {0, 0, 0, 0, 0, 2, 0, 1, 6},  {8, 11, 0, 0, 0, 0, 1, 0, 7},
+      {0, 0, 2, 0, 0, 0, 6, 7, 0}};
+  Weighted_Graph graph(table);
+  ASSERT_EQ(graph.Dijkstra_algo(1),
+            std::vector<int>({0, 4, 12, 19, 21, 11, 9, 8, 14}));
+  ASSERT_EQ(graph.Dijkstra_algo(3),
+            std::vector<int>({12, 8, 0, 7, 14, 4, 6, 7, 2}));
+}
+
+TEST(TopologySort, Simple2) {
+  std::vector<std::vector<int>> table = {
+      {0, 7, 9, 0, 0, 14},  {7, 0, 10, 15, 0, 0}, {9, 10, 0, 11, 0, 2},
+      {0, 15, 11, 0, 6, 0}, {0, 0, 0, 6, 0, 9},   {14, 0, 2, 0, 9, 0}};
+  Weighted_Graph graph(table);
+  ASSERT_EQ(graph.Dijkstra_algo(1), std::vector<int>({0, 7, 9, 20, 20, 11}));
+  ASSERT_EQ(graph.Dijkstra_algo(5), std::vector<int>({20, 21, 11, 6, 0, 9}));
+}
+
+TEST(TopologySort, Simple3) {
+  std::vector<std::vector<int>> table = {{1, 2, 3}, {1, 2, 3, 4}};
+  EXPECT_THROW(Weighted_Graph graph(table), std::logic_error);
+}
+
+TEST(TopologySort, Simple4) {
+  std::vector<std::vector<int>> table = {{}, {}, {}};
+  EXPECT_THROW(Weighted_Graph graph(table), std::logic_error);
+}
+
+TEST(TopologySort, Simple5) {
+  std::vector<std::vector<int>> table = {};
+  EXPECT_THROW(Weighted_Graph graph(table), std::logic_error);
 }
