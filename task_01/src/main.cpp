5f755f331e2bb40dd956063bc3ec36edf19631e2
Dmitry Krivoruchko <144147209+UmbrellaLeaf5@users.noreply.github.com>
2024-09-28 10:45:23 +0000

    start topology_sort

--- task_01/src/main.cpp
+++ task_01/src/main.cpp
@@ -1,3 +1,17 @@
-#include <iostream>
+#include "topology_sort.hpp"
 
-int main() { return 0; }
+int main() {
+  Graph<std::string, long> graph =
+      Graph<std::string, long>::GraphWeighted({{"A", "B", 1},
+                                               {"A", "C", 2},
+                                               {"B", "D", 3},
+                                               {"B", "E", 4},
+                                               {"C", "F", 5},
+                                               {"D", "F", 6},
+                                               {"E", "F", 7}});
+
+  auto sorted_verts = TopologySort(graph);
+  std::cout << sorted_verts << std::endl;
+
+  return 0;
+}
