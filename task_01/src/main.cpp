1e58f0e517d280cb82567834c121417a286bad1d
Dmitry Krivoruchko <144147209+UmbrellaLeaf5@users.noreply.github.com>
2024-09-30 09:06:27 +0000

    complete topological_sort for std::string

--- task_01/src/main.cpp
+++ task_01/src/main.cpp
@@ -1,6 +1,15 @@
 #include "topological_sort.hpp"
 
 int main() {
+  // ------------------------ Graph with size_t -------------------------
+
+  std::cout << std::endl
+            << "------------------------------"
+               "      Graph with size_t       "
+               "------------------------------"
+            << std::endl
+            << std::endl;
+
   auto graph = Graph<size_t, long>::GraphWeighted({{0, 1, 1},
                                                    {0, 2, 2},
                                                    {1, 3, 3},
@@ -9,25 +18,38 @@ int main() {
                                                    {3, 5, 6},
                                                    {4, 5, 7}});
 
-  // std::cout << graph << std::endl;
-  // graph.PrintAdjList();
+  std::cout << graph << std::endl << std::endl;
+  graph.PrintAdjList();
+
+  std::cout << std::endl << "ANSWER: ";
 
   auto sorted_verts = TopologicalSort(graph);
-  std::cout << sorted_verts << std::endl;
+  std::cout << sorted_verts << std::endl << std::endl;
 
-  // auto graph_strs = Graph<std::string, long>::GraphWeighted({{"0", "1", 1},
-  //                                                            {"0", "2", 2},
-  //                                                            {"1", "3", 3},
-  //                                                            {"1", "4", 4},
-  //                                                            {"2", "5", 5},
-  //                                                            {"3", "5", 6},
-  //                                                            {"4", "5", 7}});
+  // ------------------------ Graph with string -------------------------
 
-  // // std::cout << graph_strs << std::endl;
-  // // graph_strs.PrintAdjList();
+  std::cout << std::endl
+            << "------------------------------"
+               "      Graph with string       "
+               "------------------------------"
+            << std::endl
+            << std::endl;
 
-  // auto sorted_verts = TopologicalSort(graph_strs);
-  // std::cout << sorted_verts << std::endl;
+  auto graph_strs = Graph<std::string, long>::GraphWeighted({{"A", "B", 1},
+                                                             {"A", "C", 2},
+                                                             {"B", "D", 3},
+                                                             {"B", "E", 4},
+                                                             {"C", "F", 5},
+                                                             {"D", "F", 6},
+                                                             {"E", "F", 7}});
+
+  std::cout << graph_strs << std::endl << std::endl;
+  graph_strs.PrintAdjList();
+
+  std::cout << std::endl << "ANSWER: ";
+
+  auto sorted_verts_strs = TopologicalSort(graph_strs);
+  std::cout << sorted_verts_strs << std::endl << std::endl;
 
   return 0;
 }
--- task_01/src/topological_sort.hpp
+++ task_01/src/topological_sort.hpp
@@ -6,6 +6,20 @@
 #include "graph/graph.hpp"
 
 template <typename weight_t>
+static void TopologicalSortStepDFS(const Graph<size_t, weight_t>& graph,
+                                   size_t u_vert, std::vector<bool>& visited,
+                                   std::vector<size_t>& dfs_stack) {
+  visited[u_vert] = true;
+
+  for (size_t i = 0; i < graph.GetAdjList()[u_vert].size(); i++) {
+    auto vert = graph.GetAdjList()[u_vert][i];
+    if (!visited[vert]) TopologicalSortStepDFS(graph, vert, visited, dfs_stack);
+  }
+
+  dfs_stack.push_back(u_vert);
+}
+
+template <typename weight_t>
 std::vector<size_t> TopologicalSort(const Graph<size_t, weight_t>& graph) {
   std::vector<bool> visited(graph.VertsAmount(), false);
   std::vector<size_t> ans;
@@ -19,15 +33,33 @@ std::vector<size_t> TopologicalSort(const Graph<size_t, weight_t>& graph) {
 }
 
 template <typename weight_t>
-static void TopologicalSortStepDFS(const Graph<size_t, weight_t>& graph,
-                                   size_t u_vert, std::vector<bool>& visited,
-                                   std::vector<size_t>& dfs_stack) {
+static void TopologicalSortStepDFS(
+    const Graph<std::string, weight_t>& graph, std::string u_vert,
+    std::unordered_map<std::string, bool>& visited,
+    std::vector<std::string>& dfs_stack) {
   visited[u_vert] = true;
 
-  for (size_t i = 0; i < graph.GetAdjList()[u_vert].size(); i++) {
-    auto vert = graph.GetAdjList()[u_vert][i];
+  for (size_t i = 0; i < graph.GetAdjListWithKeys()[u_vert].size(); i++) {
+    auto vert = graph.GetAdjListWithKeys()[u_vert][i];
     if (!visited[vert]) TopologicalSortStepDFS(graph, vert, visited, dfs_stack);
   }
 
   dfs_stack.push_back(u_vert);
+}
+
+template <typename weight_t>
+std::vector<std::string> TopologicalSort(
+    const Graph<std::string, weight_t>& graph) {
+  std::unordered_map<std::string, bool> visited;
+
+  for (const auto& vert : graph.Verts()) visited[vert] = false;
+
+  std::vector<std::string> ans;
+
+  for (const auto& vert : graph.Verts())
+    if (!visited[vert]) TopologicalSortStepDFS(graph, vert, visited, ans);
+
+  std::reverse(ans.begin(), ans.end());
+
+  return ans;
 }
