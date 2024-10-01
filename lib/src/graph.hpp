6e15ed6d6e7dfac310b8cde190a0e44fcc84c82c
Sukhanov George <sukhanov.ge@phystech.edu>
2024-10-01 20:33:37 +0000

    task_02 done

--- lib/src/graph.hpp
+++ lib/src/graph.hpp
@@ -131,3 +131,75 @@ class Weighted_Graph {
     return altered_graph;
   }
 };
+
+template <typename T>
+class BridgeGraph {
+ private:
+  int vertices_number;
+  std::list<T>* adjacency_list;
+  vector<int> tin, low;
+  vector<bool> visited;
+  vector<std::pair<int, int>> bridges;
+  vector<int> articulation_points;
+
+  void dfs(int v, int p, int& timer) {
+    visited[v] = true;
+    tin[v] = low[v] = timer++;
+    int children = 0;
+
+    for (int to : adjacency_list[v]) {
+      if (to == p) continue;
+      if (visited[to]) {
+        low[v] = std::min(low[v], tin[to]);
+      } else {
+        dfs(to, v, timer);
+        low[v] = std::min(low[v], low[to]);
+
+        if (low[to] > tin[v]) bridges.push_back({v, to});
+        if (low[to] >= tin[v] && p != -1) {
+          if (articulation_points.empty()) {
+            articulation_points.push_back(v);
+          } else if (articulation_points.back() != v) {
+            articulation_points.push_back(v);
+          }
+        }
+        ++children;
+      }
+    }
+
+    if (p == -1 && children > 1) articulation_points.push_back(v);
+  }
+
+ public:
+  BridgeGraph(int number) {
+    if (number <= 0) throw std::logic_error("number must be positive!");
+    vertices_number = number;
+    adjacency_list = new std::list<T>[vertices_number + 1];
+  }
+
+  void AddEdge(int first_verticle, int second_verticle) {
+    if (first_verticle < 0 || first_verticle > vertices_number ||
+        second_verticle < 0 || second_verticle > vertices_number)
+      throw std::logic_error("such node does not exist");
+    if (first_verticle == second_verticle)
+      throw std::logic_error("graph must be acyclic!");
+    adjacency_list[first_verticle].push_back(second_verticle);
+    adjacency_list[second_verticle].push_back(first_verticle);
+  }
+
+  void FindBridgesAndArticulationPoints() {
+    int timer = 0;
+    tin.assign(vertices_number + 1, -1);
+    low.assign(vertices_number + 1, -1);
+    visited.assign(vertices_number + 1, false);
+    bridges.clear();
+    articulation_points.clear();
+
+    for (int i = 0; i <= vertices_number; ++i) {
+      if (!visited[i]) dfs(i, -1, timer);
+    }
+  }
+
+  vector<std::pair<int, int>> GiveBridges() { return bridges; }
+  vector<int> GivePoints() { return articulation_points; }
+};
--- task_02/src/test.cpp
+++ task_02/src/test.cpp
@@ -1,42 +1,73 @@
 
 #include <gtest/gtest.h>
 
-#include <stack>
+#include <graph.hpp>
 
-#include "stack.hpp"
+using std::pair;
 
-TEST(StackTest, Simple) {
-  Stack stack;
-  stack.Push(1);              // Stack [1]
-  ASSERT_EQ(stack.Pop(), 1);  // Stack []
-  stack.Push(1);              // Stack [1]
-  stack.Push(2);              // Stack [1, 2]
-  ASSERT_EQ(stack.Pop(), 2);  // Stack [1]
-  ASSERT_EQ(stack.Pop(), 1);  // Stack []
-  stack.Push(1);              // Stack [1]
-  stack.Push(2);              // Stack [1, 2]
-  ASSERT_EQ(stack.Pop(), 2);  // Stack [1]
-  stack.Push(3);              // Stack [1, 3]
-  ASSERT_EQ(stack.Pop(), 3);  // Stack [1]
-  ASSERT_EQ(stack.Pop(), 1);  // Stack []
+TEST(StackTest, test1) {
+  BridgeGraph<int> g(5);
+
+  g.AddEdge(1, 2);
+  g.AddEdge(1, 3);
+  g.AddEdge(3, 4);
+  g.AddEdge(3, 5);
+  g.AddEdge(4, 5);
+
+  g.FindBridgesAndArticulationPoints();
+
+  vector<pair<int, int>> bridges({{1, 2}, {1, 3}});
+  ASSERT_EQ(g.GiveBridges(), bridges);
+  vector<int> articulation_points({3, 1});
+  ASSERT_EQ(g.GivePoints(), articulation_points);
 }
 
-TEST(MinStackTest, Simple) {
-  MinStack stack;
-  stack.Push(1);  // Stack [1]
-  ASSERT_EQ(stack.GetMin(), 1);
-  ASSERT_EQ(stack.Pop(), 1);  // Stack []
-  stack.Push(1);              // Stack [1]
-  stack.Push(2);              // Stack [1, 2]
-  ASSERT_EQ(stack.GetMin(), 1);
-  ASSERT_EQ(stack.Pop(), 2);  // Stack [1]
-  ASSERT_EQ(stack.Pop(), 1);  // Stack []
-  stack.Push(1);              // Stack [1]
-  stack.Push(2);              // Stack [1, 2]
-  ASSERT_EQ(stack.GetMin(), 1);
-  ASSERT_EQ(stack.Pop(), 2);  // Stack [1]
-  stack.Push(3);              // Stack [1, 3]
-  ASSERT_EQ(stack.GetMin(), 1);
-  ASSERT_EQ(stack.Pop(), 3);  // Stack [1]
-  ASSERT_EQ(stack.Pop(), 1);  // Stack []
+TEST(StackTest, test2) {
+  BridgeGraph<int> g(5);
+
+  g.AddEdge(1, 5);
+  g.AddEdge(1, 2);
+  g.AddEdge(1, 3);
+  g.AddEdge(2, 4);
+  g.AddEdge(2, 5);
+  g.AddEdge(3, 5);
+
+  g.FindBridgesAndArticulationPoints();
+
+  vector<pair<int, int>> bridges({{2, 4}});
+  ASSERT_EQ(g.GiveBridges(), bridges);
+  vector<int> articulation_points({2});
+  ASSERT_EQ(g.GivePoints(), articulation_points);
+}
+
+TEST(StackTest, test3) {
+  BridgeGraph<int> g(8);
+
+  g.AddEdge(1, 2);
+  g.AddEdge(1, 8);
+  g.AddEdge(2, 6);
+  g.AddEdge(2, 8);
+  g.AddEdge(3, 4);
+  g.AddEdge(3, 5);
+  g.AddEdge(3, 6);
+  g.AddEdge(6, 7);
+  g.AddEdge(7, 8);
+
+  g.FindBridgesAndArticulationPoints();
+
+  vector<pair<int, int>> bridges({{3, 4}, {3, 5}, {6, 3}});
+  ASSERT_EQ(g.GiveBridges(), bridges);
+  vector<int> articulation_points({3, 6});
+  ASSERT_EQ(g.GivePoints(), articulation_points);
+}
+
+TEST(StackTest, test4) {
+  EXPECT_THROW(BridgeGraph<int> g(-5), std::logic_error);
+  EXPECT_THROW(BridgeGraph<int> g(0), std::logic_error);
+}
+
+TEST(StackTest, test5) {
+  BridgeGraph<int> g(2);
+  EXPECT_THROW(g.AddEdge(5, 6), std::logic_error);
+  EXPECT_THROW(g.AddEdge(5, -4), std::logic_error);
 }
