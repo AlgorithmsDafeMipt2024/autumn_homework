d430b291da1cfef7e0680eadbbd1755f182086a3
Sukhanov George <sukhanov.ge@phystech.edu>
2024-10-02 15:12:07 +0000

    task_06 done

---
+++ task_06/src/algo.hpp
@@ -0,0 +1,62 @@
+#include <algorithm>
+#include <cmath>
+#include <stdexcept>
+#include <vector>
+
+using std::vector;
+
+/*
+Алгоритм Фарака-Колтона и Бендера 
+Выдает запрос за O(1), предобработка структуры данных - O(n)
+*/
+
+class LCA {
+ public:
+  LCA(const vector<vector<int>>& tree, int root) {
+    if (tree.empty()) throw std::logic_error("the input vector is empty!");
+    for (const auto& i : tree)
+      if (i.empty()) throw std::logic_error("vertex vector is empty");
+    n = tree.size();
+    int log_n = log2(n) + 1;
+    up.assign(n, vector<int>(log_n, -1));
+    depth.resize(n);
+    Dfs(tree, root, root);
+  }
+
+  int Query(int u, int v) {
+    if (u < 0 || v >= n) throw std::out_of_range("incorrect boundaries!");
+    if (depth[u] < depth[v]) std::swap(u, v);
+    int log_n = up[0].size();
+    for (int i = log_n - 1; i >= 0; i--) {
+      if (depth[u] - (1 << i) >= depth[v]) u = up[u][i];
+    }
+    if (u == v) return u;
+    for (int i = log_n - 1; i >= 0; i--) {
+      if (up[u][i] != up[v][i]) {
+        u = up[u][i];
+        v = up[v][i];
+      }
+    }
+    return up[u][0];
+  }
+
+ private:
+  vector<vector<int>> up;
+  vector<int> depth;
+  int n;
+
+  void Dfs(const vector<vector<int>>& tree, int v, int p) {
+    up[v][0] = p;
+    for (int i = 1; i < up[v].size(); i++) {
+      if (up[v][i - 1] != -1) {
+        up[v][i] = up[up[v][i - 1]][i - 1];
+      }
+    }
+    for (int u : tree[v]) {
+      if (u != p) {
+        depth[u] = depth[v] + 1;
+        Dfs(tree, u, v);
+      }
+    }
+  }
+};
--- task_06/src/test.cpp
+++ task_06/src/test.cpp
@@ -1,6 +1,104 @@
-
 #include <gtest/gtest.h>
 
-TEST(TopologySort, Simple) {
-  ASSERT_EQ(1, 1);  // Stack []
+#include "algo.hpp"
+
+TEST(TopologySort, test1) {
+  vector<vector<int>> tree(10);
+  tree[0] = {1, 2};
+  tree[1] = {0, 3, 4};
+  tree[2] = {0, 5, 6};
+  tree[3] = {1, 7};
+  tree[4] = {1, 8, 9};
+  tree[5] = {2};
+  tree[6] = {2};
+  tree[7] = {3};
+  tree[8] = {4};
+  tree[9] = {4};
+
+  LCA lca(tree, 0);
+
+  ASSERT_EQ(lca.Query(3, 4), 1);
+  ASSERT_EQ(lca.Query(7, 8), 1);
+  ASSERT_EQ(lca.Query(5, 6), 2);
+  ASSERT_EQ(lca.Query(7, 9), 1);
+  ASSERT_EQ(lca.Query(8, 9), 4);
+}
+
+TEST(TopologySort, test2) {
+  vector<vector<int>> tree(7);
+  tree[0] = {1, 2};
+  tree[1] = {0, 3, 4};
+  tree[2] = {0, 5, 6};
+  tree[3] = {1};
+  tree[4] = {1};
+  tree[5] = {2};
+  tree[6] = {2};
+
+  LCA lca(tree, 0);
+
+  ASSERT_EQ(lca.Query(3, 4), 1);
+  ASSERT_EQ(lca.Query(3, 5), 0);
+  ASSERT_EQ(lca.Query(4, 6), 0);
+  ASSERT_EQ(lca.Query(5, 6), 2);
+  ASSERT_EQ(lca.Query(1, 2), 0);
+}
+
+TEST(TopologySort, test3) {
+  vector<vector<int>> tree(9);
+  tree[0] = {1, 2};
+  tree[1] = {0, 3, 4};
+  tree[2] = {0, 5, 6};
+  tree[3] = {1, 7};
+  tree[4] = {1, 8};
+  tree[5] = {2};
+  tree[6] = {2};
+  tree[7] = {3};
+  tree[8] = {4};
+
+  LCA lca(tree, 0);
+
+  ASSERT_EQ(lca.Query(3, 4), 1);
+  ASSERT_EQ(lca.Query(7, 8), 1);
+  ASSERT_EQ(lca.Query(5, 6), 2);
+  ASSERT_EQ(lca.Query(7, 3), 3);
+  ASSERT_EQ(lca.Query(8, 4), 4);
+}
+
+TEST(TopologySort, test4) {
+  vector<vector<int>> tree(4);
+  tree[0] = {1, 2};
+  tree[1] = {0, 3};
+  tree[2] = {0};
+  tree[3] = {1};
+
+  LCA lca(tree, 0);
+
+  EXPECT_THROW(lca.Query(-1, 2), std::out_of_range);
+}
+
+TEST(TopologySort, test5) {
+  vector<vector<int>> tree(4);
+  tree[0] = {1, 2};
+  tree[1] = {0, 3};
+  tree[2] = {0};
+  tree[3] = {1};
+
+  LCA lca(tree, 0);
+
+  EXPECT_THROW(lca.Query(0, 4), std::out_of_range);
+}
+
+TEST(TopologySort, test6) {
+  vector<vector<int>> tree(4);
+  tree[0] = {1, 2};
+  tree[1] = {0, 3};
+  tree[2] = {0};
+
+  EXPECT_THROW(LCA lca(tree, 0), std::logic_error);
+}
+
+TEST(TopologySort, test7) {
+  vector<vector<int>> tree;
+
+  EXPECT_THROW(LCA lca(tree, 0), std::logic_error);
 }
