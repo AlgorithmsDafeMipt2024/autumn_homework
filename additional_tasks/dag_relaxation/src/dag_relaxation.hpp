ffd686dceefc863912c6a7b0ca70e76ac319c649
Dmitry Krivoruchko <144147209+UmbrellaLeaf5@users.noreply.github.com>
2024-10-25 17:10:13 +0000

    primary complete dag_relaxation

--- additional_tasks/dag_relaxation/src/dag_relaxation.hpp
+++ additional_tasks/dag_relaxation/src/dag_relaxation.hpp
@@ -1,3 +1,56 @@
 #pragma once
 
-#include "graph/graph.hpp"
+#include <limits>
+#include <queue>
+
+#include "topological_sort.hpp"
+
+/**
+ * @brief Вычисляет кратчайшие пути от заданной начальной вершины до всех других
+ * вершин в направленном ациклическом графе (DAG) с помощью релаксации.
+ * @tparam vert_t: тип вершины в графе
+ * @tparam weight_t: тип веса в графе
+ * @param graph: граф, для которого необходимо вычислить кратчайшие пути.
+ * @param start_vert: начальная вершина, от которой вычисляются расстояния.
+ * @throw std::invalid_argument("DAGRelaxation: there is no such start
+ * vertice.")
+ * @return std::unordered_map<vert_t, weight_t>: словарь, где ключ - вершина, а
+ * значение - кратчайшее расстояние от start_vert до этой вершины
+ * (если до вершины нет пути, то значение будет равно
+ * std::numeric_limits<weight_t>::max())
+ */
+template <typename vert_t, typename weight_t>
+std::unordered_map<vert_t, weight_t> DAGRelaxation(
+    const Graph<vert_t, weight_t>& graph, vert_t start_vert) {
+  if (std::find(graph.Verts().begin(), graph.Verts().end(), start_vert) ==
+      graph.Verts().end())
+    throw std::invalid_argument(
+        "DAGRelaxation: there is no such start vertice.");
+
+  std::unordered_map<vert_t, weight_t> distances;
+
+  // инициализация расстояний до бесконечности
+  for (const auto& vert : graph.Verts())
+    distances[vert] = std::numeric_limits<weight_t>::max();
+
+  // расстояние от начальной вершины до самой себя равно 0
+  distances[start_vert] = 0;
+
+  std::vector<vert_t> sorted_verts = TopologicalSort(graph);
+
+  for (const auto& u_vert : sorted_verts)
+    for (size_t i = 0; i < graph.GetAdjList()[u_vert].size(); i++) {
+      auto vert = graph.GetAdjList()[u_vert][i];
+      // (нас не интересует бесконечное расстояние от start до u_vert)
+      if (distances[u_vert] == std::numeric_limits<weight_t>::max()) continue;
+
+      // релаксируем ребро, если текущее расстояние до vert больше, чем
+      // расстояние до u_vert плюс вес ребра
+      if (distances[vert] >
+          distances[u_vert] + graph.GetWeightOfEdge({u_vert, vert}))
+        distances[vert] =
+            distances[u_vert] + graph.GetWeightOfEdge({u_vert, vert});
+    }
+
+  return distances;
+}
--- additional_tasks/dag_relaxation/src/main.cpp
+++ additional_tasks/dag_relaxation/src/main.cpp
@@ -1,3 +1,28 @@
 #include "dag_relaxation.hpp"
 
-int main() { return 0; }
+int main() {
+  Graph<std::string, long> graph;
+  graph.AddEdge("A", "B", 5);
+  graph.AddEdge("A", "C", 3);
+  graph.AddEdge("B", "D", 2);
+  graph.AddEdge("C", "D", 4);
+  graph.AddEdge("C", "E", 1);
+  graph.AddEdge("D", "F", 7);
+  graph.AddEdge("E", "F", 2);
+  graph.MakeDirected();
+
+  std::string start = "F";
+
+  auto distances = DAGRelaxation(graph, start);
+
+  std::cout << "кратчайшие пути от вершины '" << start << "':" << std::endl;
+  for (const auto& vert : distances) {
+    std::cout << "до вершины '" << vert.first << "': "
+              << (vert.second == std::numeric_limits<long>::max()
+                      ? "inf"
+                      : std::to_string(vert.second))
+              << std::endl;
+  }
+
+  return 0;
+}
