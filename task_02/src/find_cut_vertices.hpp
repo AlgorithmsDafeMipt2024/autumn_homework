61d327e24d6bf3bbcef2f530d1b2f3ee3b8e5e0e
Dmitry Krivoruchko <144147209+UmbrellaLeaf5@users.noreply.github.com>
2024-10-31 20:36:39 +0300

    add tests for FindCutVertices

--- task_02/src/find_cut_vertices.hpp
+++ task_02/src/find_cut_vertices.hpp
@@ -45,6 +45,13 @@ inline void FindCutVerticesStep(
 
 }  // namespace
 
+/**
+ * @brief Поиск точек сочленения в графе (используя элементы алгоритма Тарьяна)
+ * @tparam vert_t: тип вершин
+ * @tparam weight_t: тип весов
+ * @param graph: исходный граф
+ * @return std::vector<vert_t>: точки сочленения в графе
+ */
 template <AllowedVertType vert_t, AllowedWeightType weight_t>
 inline std::vector<vert_t> FindCutVertices(
     const Graph<vert_t, weight_t>& graph) {
