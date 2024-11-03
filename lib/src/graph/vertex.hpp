544f34902d1860cd8f45df1c99e0d86a0efc581c
MrWh1teF0x <142975991+MrWh1teF0x@users.noreply.github.com>
2024-11-03 11:22:37 +0000

    add class "Vertex"

---
+++ lib/src/graph/vertex.hpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+class Vertex {
+ public:
+  Vertex(size_t vertex_id, std::vector<size_t> adj_vertices)
+      : vertex_id(vertex_id), adjacent_vertices(adj_vertices) {}
+
+  Vertex(size_t vertex_id, std::initializer_list<size_t> adj_vertices) {
+    for (size_t adj_vertex_id : adjacent_vertices)
+      adjacent_vertices.push_back(adj_vertex_id);
+  }
+
+  void AddAdjacentVertex(size_t adj_vertex) {
+    adjacent_vertices.push_back(adj_vertex);
+  }
+
+  size_t GetVertexId() const { return vertex_id; }
+
+  std::vector<size_t> GetAdjacentVertices() const { return adjacent_vertices; }
+
+  void PrintAdjacentVertices() const {
+    std::cout << "Adjacent vertices for " << vertex_id << " vertex: { ";
+    for (size_t vertex : adjacent_vertices) std::cout << vertex << ", ";
+    std::cout << "}\n";
+  }
+
+ private:
+  size_t vertex_id;
+  std::vector<size_t> adjacent_vertices;
+};
