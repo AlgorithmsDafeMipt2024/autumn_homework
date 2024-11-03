3d1d9cbe0b655e39e7b6e03d57e21e706f050b74
MrWh1teF0x <142975991+MrWh1teF0x@users.noreply.github.com>
2024-11-03 17:09:05 +0000

    remove useless method and beautify

--- lib/src/graph/vertex.hpp
+++ lib/src/graph/vertex.hpp
@@ -5,20 +5,14 @@
 template <typename T>
 class Vertex {
  public:
-  Vertex(T vertex_id, std::vector<T> adj_vertices)
+  Vertex(const T& vertex_id, const std::vector<T>& adj_vertices = {})
       : vertex_id(vertex_id), adjacent_vertices(adj_vertices) {}
 
-  Vertex(T vertex_id, std::initializer_list<T> adj_vertices)
-      : vertex_id(vertex_id) {
-    for (T adj_vertex_id : adj_vertices)
-      adjacent_vertices.push_back(adj_vertex_id);
-  }
-
-  void AddAdjacentVertex(T adj_vertex) {
+  void AddAdjacentVertex(const T& adj_vertex) {
     adjacent_vertices.push_back(adj_vertex);
   }
 
-  void DeleteAdjacentVertex(T adj_vertex) {
+  void DeleteAdjacentVertex(const T& adj_vertex) {
     for (int i = 0; i < adjacent_vertices.size(); i++) {
       if (adjacent_vertices[i] == adj_vertex) {
         adjacent_vertices.erase(adjacent_vertices.begin() + i);
@@ -41,7 +35,7 @@ class Vertex {
     std::cout << "}\n";
   }
 
-  bool ContainsAdjacentVertex(T adj_vert) const {
+  bool ContainsAdjacentVertex(const T& adj_vert) const {
     for (T vertex : adjacent_vertices) {
       if (adj_vert == vertex) return true;
     }
