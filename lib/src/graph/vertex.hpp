bd8a8d14e9d2fdb11d0fc85884c13ff23c1566a3
MrWh1teF0x <142975991+MrWh1teF0x@users.noreply.github.com>
2024-11-03 12:45:40 +0000

    add template to class "Vertex"

--- lib/src/graph/vertex.hpp
+++ lib/src/graph/vertex.hpp
@@ -1,32 +1,33 @@
 #include <iostream>
 #include <vector>
 
+template <typename T>
 class Vertex {
  public:
-  Vertex(size_t vertex_id, std::vector<size_t> adj_vertices)
+  Vertex(T vertex_id, std::vector<T> adj_vertices)
       : vertex_id(vertex_id), adjacent_vertices(adj_vertices) {}
 
-  Vertex(size_t vertex_id, std::initializer_list<size_t> adj_vertices)
+  Vertex(T vertex_id, std::initializer_list<T> adj_vertices)
       : vertex_id(vertex_id) {
-    for (size_t adj_vertex_id : adj_vertices)
+    for (T adj_vertex_id : adj_vertices)
       adjacent_vertices.push_back(adj_vertex_id);
   }
 
-  void AddAdjacentVertex(size_t adj_vertex) {
+  void AddAdjacentVertex(T adj_vertex) {
     adjacent_vertices.push_back(adj_vertex);
   }
 
-  size_t GetVertexId() const { return vertex_id; }
+  T GetVertexId() const { return vertex_id; }
 
-  std::vector<size_t> GetAdjacentVertices() const { return adjacent_vertices; }
+  std::vector<T> GetAdjacentVertices() const { return adjacent_vertices; }
 
   void PrintAdjacentVertices() const {
-    std::cout << "Adjacent vertices for " << vertex_id << " vertex: { ";
-    for (size_t vertex : adjacent_vertices) std::cout << vertex << ", ";
+    std::cout << "Adjacent vertices for " << vertex_id << ": { ";
+    for (T vertex : adjacent_vertices) std::cout << vertex << ", ";
     std::cout << "}\n";
   }
 
  private:
-  size_t vertex_id;
-  std::vector<size_t> adjacent_vertices;
+  T vertex_id;
+  std::vector<T> adjacent_vertices;
 };
