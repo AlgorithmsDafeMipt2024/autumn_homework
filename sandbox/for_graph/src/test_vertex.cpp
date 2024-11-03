eacc7032babf6ac9af82203b287264c02af2c781
MrWh1teF0x <142975991+MrWh1teF0x@users.noreply.github.com>
2024-11-03 12:53:33 +0000

    add tests for "Vertex"

---
+++ sandbox/for_graph/src/test_vertex.cpp
@@ -0,0 +1,44 @@
+
+#include <gtest/gtest.h>
+
+#include "graph/vertex.hpp"
+
+TEST(TestVertex, Test_Constructors_1) {
+  Vertex<size_t> v1(0, {1, 5, 3, 7, 4, 8});
+  Vertex<size_t> v2(0, std::vector<size_t>{1, 5, 3, 7, 4, 8});
+
+  ASSERT_EQ(v1.GetVertexId(), v2.GetVertexId());
+
+  ASSERT_EQ(v1.GetAdjacentVertices().size(), v2.GetAdjacentVertices().size());
+
+  for (int i = 0; i < v1.GetAdjacentVertices().size(); i++) {
+    ASSERT_EQ(v1.GetAdjacentVertices()[i], v2.GetAdjacentVertices()[i]);
+  }
+}
+
+TEST(TestVertex, Test_Constructors_2) {
+  Vertex<char> v1('A', {'B', 'C', 'E', 'D', 'G'});
+  Vertex<char> v2('A', std::vector<char>{'B', 'C', 'E', 'D', 'G'});
+
+  ASSERT_EQ(v1.GetVertexId(), v2.GetVertexId());
+
+  ASSERT_EQ(v1.GetAdjacentVertices().size(), v2.GetAdjacentVertices().size());
+
+  for (int i = 0; i < v1.GetAdjacentVertices().size(); i++) {
+    ASSERT_EQ(v1.GetAdjacentVertices()[i], v2.GetAdjacentVertices()[i]);
+  }
+}
+
+TEST(TestVertex, Test_Constructors_3) {
+  Vertex<std::string> v1("A", {"B", "C", "E", "D", "G"});
+  Vertex<std::string> v2("A",
+                         std::vector<std::string>{"B", "C", "E", "D", "G"});
+
+  ASSERT_EQ(v1.GetVertexId(), v2.GetVertexId());
+
+  ASSERT_EQ(v1.GetAdjacentVertices().size(), v2.GetAdjacentVertices().size());
+
+  for (int i = 0; i < v1.GetAdjacentVertices().size(); i++) {
+    ASSERT_EQ(v1.GetAdjacentVertices()[i], v2.GetAdjacentVertices()[i]);
+  }
+}
