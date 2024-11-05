72f11b1b9b574ffce51e9f55afb567f1cb225e72
MrWh1teF0x <142975991+MrWh1teF0x@users.noreply.github.com>
2024-11-05 22:41:29 +0000

    add class "WeightedGraph"

---
+++ lib/src/graph/weighted_graph.hpp
@@ -0,0 +1,106 @@
+#pragma once
+#include <limits>
+
+#include "graph.hpp"
+
+template <typename T>
+struct WeightedEdge {
+ public:
+  WeightedEdge(const T& start_vertex, const T& end_vertex, int weight)
+      : start_vertex(start_vertex), end_vertex(end_vertex), weight(weight) {}
+
+  T start_vertex;
+  T end_vertex;
+  int weight;
+};
+
+template <typename T>
+class WeightedGraph {
+ public:
+  WeightedGraph(const std::vector<Vertex<T>>& vertices,
+                const std::vector<WeightedEdge<T>>& w_edges)
+      : graph(vertices), weighted_edges(w_edges) {
+    if (graph.GetEdgesCount() < w_edges.size())
+      throw std::invalid_argument(
+          "The number of weighted edges is greater than the number of all "
+          "edges in the graph!");
+
+    for (int i = 0; i < weighted_edges.size(); i++) {
+      bool flag = true;
+      for (int j = 0; j < graph.GetVerticesCount(); j++) {
+        Vertex<T> vertex = graph.GetVertices()[j];
+        if (weighted_edges[i].state_vertex == vertex.GetVertexId() &&
+            == vertex.ContainsAdjVertex(weighted_edges[i].end_vertex)) {
+          flag = false;
+          break;
+        }
+      }
+      if (flag)
+        throw std::invalid_argument("There is no such edge in a graph!");
+    }
+  }
+
+  WeightedGraph(const std::vector<std::pair<std::pair<T, T>, int>>& w_edges) {
+    for (auto w_edge : w_edges) {
+      auto edge = w_edge.first;
+      graph.AddEdge(edge.first, edge.second);
+      weighted_edges.push_back(
+          WeightedEdge<T>(edge.first, edge.second, w_edge.second));
+    }
+  }
+
+  WeightedGraph(const std::vector<std::pair<T, T>>& edges,
+                const std::vector<int> weights) {
+    if (edges.size() != weights.size())
+      throw std::invalid_argument(
+          "The number of weighted edges is greater than the number of all "
+          "edges in the graph!");
+    for (int i = 0; i < edges.size(); i++) {
+      graph.AddEdge(edges[i].first, edges[i].second);
+      weighted_edges.push_back(
+          WeightedEdge<T>(edges[i].first, edges[i].second, weights[i]));
+    }
+  }
+
+  std::vector<WeightedEdge<T>> GetWeightedEdges() const {
+    return weighted_edges;
+  }
+
+  std::vector<Vertex<T>> GetVertices() const { return graph.GetVertices(); }
+
+  size_t GetVerticesCount() const { return graph.GetVerticesCount(); }
+
+  size_t GetEdgesCount() const { return graph.GetEdgesCount(); }
+
+  void AddWeightedEdge(const T& start, const T& end, int weight = 0) {
+    graph.AddEdge(start, end);
+    weighted_edges.push_back(WeightedEdge<T>(start, end, weight));
+  }
+
+  void AddVertex(const T& vertex) { graph.AddVertex(vertex); }
+
+  bool ContainsVertex(const T& vertex) const {
+    return graph.ContainsVertex(vertex;)
+  }
+
+  bool ContainsEdge(const T& start, const T& end) const {
+    return graph.ContainsEdge(start, end);
+  }
+
+  void SetWeightToEdge(const T& start, const T& end, int w) {
+    if (!graph.ContainsEdge(start, end))
+      throw std::invalid_argument("There is no such edge in a graph!");
+
+    for (int i = 0; i < weighted_edges.size()) {
+      if (weighted_edges[i].start_vertex == start &&
+          weighted_edges[i].end_vertex == end) {
+        weighted_edges[i].weight = w;
+        return;
+      }
+    }
+  }
+
+ private:
+  Graph<T> graph;
+  std::vector<WeightedEdge<T>> weighted_edges;
+};
