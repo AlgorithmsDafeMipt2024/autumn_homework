5a9472c12bb3371d23240a8fe63b184c46c635c2
Большаков Матвей Александрович <143873323+Matvey-cmd@users.noreply.github.com>
2024-11-11 17:50:38 +0000

    clang format

--- task_01/src/test.cpp
+++ task_01/src/test.cpp
@@ -8,8 +8,7 @@ TEST(SimpleGraph, Simple) {
   std::vector<std::pair<int, int>> edge = {{1, 2}, {2, 3}};
 
   std::vector<int> result = TopSort(vertices, edges, edge);
-  std::vector<int> expected = {
-      1, 2, 3};  
+  std::vector<int> expected = {1, 2, 3};
 
   ASSERT_EQ(result, expected);
 }
@@ -18,7 +17,7 @@ TEST(GraphWithCycle, Simple) {
   int vertices = 3;
   int edges = 3;
   std::vector<std::pair<int, int>> edge = {{1, 2}, {2, 3}, {3, 1}};
-  
+
   EXPECT_THROW(TopSort(vertices, edges, edge);, CycleGraph);
 }
 
@@ -28,7 +27,7 @@ TEST(DisconnectedGraph, Simple) {
   std::vector<std::pair<int, int>> edge = {{1, 2}, {3, 4}};
 
   std::vector<int> result = TopSort(vertices, edges, edge);
-  std::vector<int> expected ={3, 4, 1, 2};
+  std::vector<int> expected = {3, 4, 1, 2};
 
   ASSERT_EQ(result, expected);
 }
@@ -45,61 +44,64 @@ TEST(SingleVertex, Simple) {
 }
 
 TEST(MultipleValidOrders, Simple) {
-    int vertices = 4;
-    int edges = 3;
-    std::vector<std::pair<int, int>> edge = {{1, 2}, {1, 3}, {3, 4}};
-    
-    std::vector<int> result = TopSort(vertices, edges, edge);
-    std::vector<int> expected1 = {1, 3, 4, 2};
-    
-    ASSERT_EQ(result, expected1);
+  int vertices = 4;
+  int edges = 3;
+  std::vector<std::pair<int, int>> edge = {{1, 2}, {1, 3}, {3, 4}};
+
+  std::vector<int> result = TopSort(vertices, edges, edge);
+  std::vector<int> expected1 = {1, 3, 4, 2};
+
+  ASSERT_EQ(result, expected1);
 }
 
 TEST(AllVerticesConnected, Simple) {
-    int vertices = 5;
-    int edges = 4;
-    std::vector<std::pair<int, int>> edge = {{1, 2}, {2, 3}, {3, 4}, {4, 5}};
-    
-    std::vector<int> result = TopSort(vertices, edges, edge);
-    std::vector<int> expected = {1, 2, 3, 4, 5};
-    
-    ASSERT_EQ(result, expected);
+  int vertices = 5;
+  int edges = 4;
+  std::vector<std::pair<int, int>> edge = {{1, 2}, {2, 3}, {3, 4}, {4, 5}};
+
+  std::vector<int> result = TopSort(vertices, edges, edge);
+  std::vector<int> expected = {1, 2, 3, 4, 5};
+
+  ASSERT_EQ(result, expected);
 }
 
 TEST(ReverseOrder, Simple) {
-    int vertices = 3;
-    int edges = 2;
-    std::vector<std::pair<int, int>> edge = {{3, 2}, {2, 1}};
-    
-    std::vector<int> result = TopSort(vertices, edges, edge);
-    std::vector<int> expected = {3, 2, 1};
-    
-    ASSERT_EQ(result, expected);
+  int vertices = 3;
+  int edges = 2;
+  std::vector<std::pair<int, int>> edge = {{3, 2}, {2, 1}};
+
+  std::vector<int> result = TopSort(vertices, edges, edge);
+  std::vector<int> expected = {3, 2, 1};
+
+  ASSERT_EQ(result, expected);
 }
 
 TEST(ComplexGraph, Simple) {
-    int vertices = 6;
-    int edges = 6;
-    std::vector<std::pair<int, int>> edge = {{5, 2}, {5, 0}, {4, 0}, {4, 1}, {2, 3}, {3, 1}};
-    
-    std::vector<int> result = TopSort(vertices, edges, edge);
-    std::vector<int> expected = {6, 5, 4, 0, 2, 3, 1};
+  int vertices = 6;
+  int edges = 6;
+  std::vector<std::pair<int, int>> edge = {{5, 2}, {5, 0}, {4, 0},
+                                           {4, 1}, {2, 3}, {3, 1}};
 
-    ASSERT_EQ(result, expected);
+  std::vector<int> result = TopSort(vertices, edges, edge);
+  std::vector<int> expected = {6, 5, 4, 0, 2, 3, 1};
+
+  ASSERT_EQ(result, expected);
 }
 
 TEST(GraphWithMultipleCyclesm, Simple) {
-    int vertices = 4;
-    int edges = 5;
-    std::vector<std::pair<int, int>> edge = {{1, 2}, {2, 3}, {3, 1}, {2, 4}, {4, 2}};
-    
-    EXPECT_THROW(TopSort(vertices, edges, edge), CycleGraph);
+  int vertices = 4;
+  int edges = 5;
+  std::vector<std::pair<int, int>> edge = {
+      {1, 2}, {2, 3}, {3, 1}, {2, 4}, {4, 2}};
+
+  EXPECT_THROW(TopSort(vertices, edges, edge), CycleGraph);
 }
 
-TEST(MediumGraph, Simple){
+TEST(MediumGraph, Simple) {
   int vertices = 6;
   int edges = 6;
-  std::vector<std::pair<int, int>> edge = {{5, 2}, {4, 2}, {3, 4}, {6, 3}, {6, 1}, {5, 1}};
+  std::vector<std::pair<int, int>> edge = {{5, 2}, {4, 2}, {3, 4},
+                                           {6, 3}, {6, 1}, {5, 1}};
   std::vector<int> result = TopSort(vertices, edges, edge);
   std::vector<int> expected = {6, 5, 3, 4, 2, 1};
 
--- task_01/src/top_sort.hpp
+++ task_01/src/top_sort.hpp
@@ -3,7 +3,8 @@
 #define int long
 
 class CycleGraph : public std::logic_error {
-    using std::logic_error::logic_error;
+  using std::logic_error::logic_error;
 };
 
-std::vector<int> TopSort(int vertices, int edges, std::vector<std::pair<int, int>>& edge);
+std::vector<int> TopSort(int vertices, int edges,
+                         std::vector<std::pair<int, int>>& edge);
