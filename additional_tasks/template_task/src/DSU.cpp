da6c3bd1a8e9682374d27d1843faa1a3813ace99
Большаков Матвей Александрович <143873323+Matvey-cmd@users.noreply.github.com>
2024-11-11 16:03:02 +0000

    algotithm DSU

---
+++ additional_tasks/template_task/src/DSU.cpp
@@ -0,0 +1,67 @@
+#include "DCU.hpp"
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+DSU::DSU(int n) : parent(n), rank(n, 0) {
+  for (int i = 0; i < n; i++) parent[i] = i;
+}
+
+int DSU::find(int x) {
+  if (parent[x] != x) {
+    parent[x] = find(parent[x]);  // Path compression
+  }
+  return parent[x];
+}
+
+void DSU::unite(int x, int y) {
+  int rootX = find(x);
+  int rootY = find(y);
+  if (rootX != rootY) {
+    if (rank[rootX] > rank[rootY]) {
+      parent[rootY] = rootX;
+    } else if (rank[rootX] < rank[rootY]) {
+      parent[rootX] = rootY;
+    } else {
+      parent[rootY] = rootX;
+      rank[rootX]++;
+    }
+  }
+}
+
+bool Edge::operator<(const Edge& other) const { return weight < other.weight; }
+
+int kruskalMST(int n, vector<Edge>& edges) {
+  sort(edges.begin(), edges.end());
+  DSU dsu(n);
+  int mstWeight = 0, edgesUsed = 0;
+
+  for (const Edge& edge : edges) {
+    if (dsu.find(edge.u) != dsu.find(edge.v)) {
+      dsu.unite(edge.u, edge.v);
+      mstWeight += edge.weight;
+      edgesUsed++;
+      if (edgesUsed == n - 1) break;
+    }
+  }
+
+  return (edgesUsed == n - 1) ? mstWeight : -1;  // -1 if MST is not possible
+}
+
+int main_part(int& n, int& m, vector<vector<int>>& vec) {
+  vector<Edge> edges(m);
+  for (int i = 0; i < m; i++) {
+    edges[i].u = vec[i][0] - 1;  // Уменьшаем на 1 для 0-индексации
+    edges[i].v = vec[i][1] - 1;  // Уменьшаем на 1 для 0-индексации
+    edges[i].weight = vec[i][2];
+  }
+
+  int result = kruskalMST(n, edges);
+  if (result != -1) {
+    return result;
+  } else {
+    throw NotMST("MST does not exist");
+  }
+  return 0;
+}
--- additional_tasks/template_task/src/test.cpp
+++ additional_tasks/template_task/src/test.cpp
@@ -1,8 +1,61 @@
-
 #include <gtest/gtest.h>
 
-#include <stack>
+#include "DCU.hpp"
 
-#include "utils.hpp"
+using namespace std;
 
-TEST(Template, Simple) { ASSERT_EQ(true, true); }
+TEST(Template, Simple) {
+  int vertices = 3;
+  int edge = 3;
+  vector<vector<int>> edges = {{1, 2, 4}, {2, 3, 5}, {1, 3, 3}};
+
+  int result = main_part(vertices, edge, edges);
+  int expected = 7;
+  ASSERT_EQ(result, expected);
+}
+
+TEST(Error, Simple) {
+  int vertices = 3;
+  int edge = 1;
+  vector<vector<int>> edges = {{1, 2, 4}};
+
+  EXPECT_THROW(main_part(vertices, edge, edges), NotMST);
+}
+
+TEST(Template, NoEdges) {
+  int vertices = 5;
+  int edge = 0;
+  vector<vector<int>> edges = {};
+
+  EXPECT_THROW(main_part(vertices, edge, edges), NotMST);
+}
+
+TEST(Template, MultipleEdgesSameWeight) {
+  int vertices = 4;
+  int edge = 5;
+  vector<vector<int>> edges = {
+      {1, 2, 1}, {2, 3, 1}, {3, 4, 1}, {1, 4, 1}, {2, 4, 1}};
+
+  int result = main_part(vertices, edge, edges);
+  int expected = 3;  // MST can include edges with weight 1
+  ASSERT_EQ(result, expected);
+}
+
+TEST(Template, DisconnectedGraph) {
+  int vertices = 4;
+  int edge = 2;
+  vector<vector<int>> edges = {{1, 2, 1}, {3, 4, 1}};
+
+  EXPECT_THROW(main_part(vertices, edge, edges), NotMST);
+}
+
+TEST(Template, LargerGraph) {
+  int vertices = 6;
+  int edge = 8;
+  vector<vector<int>> edges = {{1, 2, 4}, {1, 3, 1}, {2, 3, 2}, {2, 4, 5},
+                               {3, 4, 8}, {4, 5, 3}, {5, 6, 7}, {4, 6, 6}};
+
+  int result = main_part(vertices, edge, edges);
+  int expected = 17;  // Minimum spanning tree weight
+  ASSERT_EQ(result, expected);
+}
