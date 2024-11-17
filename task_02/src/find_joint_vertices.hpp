ce4e1174a3bd1174eeb6924862059146df5fcfd6
MrWh1teF0x <142975991+MrWh1teF0x@users.noreply.github.com>
2024-11-17 15:09:16 +0000

    add finding joint vertices in graph for task_02

---
+++ task_02/src/find_joint_vertices.hpp
@@ -0,0 +1,50 @@
+#include <unordered_map>
+
+#include "graph/graph.hpp"
+
+template <typename T>
+void DFSForJointVertices(std::unordered_map<T, int>& d,
+                         std::unordered_map<T, int>& h,
+                         std::unordered_map<T, bool>& visited,
+                         std::vector<T>& joint_vertices, Graph<T>& graph,
+                         const T& v, const T& p = T()) {
+  visited[v] = true;
+  if (p) d[v] = h[v] = h[p] + 1;
+
+  int children_count = 0;
+
+  for (auto u : graph.GetAdjVertices(v)) {
+    if (u != p) {
+      if (visited[u])
+        d[v] = std::min(d[v], h[u]);
+      else {
+        DFSForJointVertices(d, h, visited, joint_vertices, graph, u, v);
+
+        d[v] = std::min(d[v], d[u]);
+        if (h[v] <= d[u] && p) joint_vertices.push_back(v);
+
+        children_count++;
+      }
+    }
+  }
+  // Отдельно проверяем корень
+  if (!p && children_count > 1) joint_vertices.push_back(v);
+}
+
+template <typename T>
+std::vector<T> FindJointVertices(Graph<T> graph) {
+  if (graph.IsOriented()) throw std::invalid_argument("Graph is oriented!");
+
+  std::unordered_map<T, int> d, h;
+  std::unordered_map<T, bool> visited;
+  for (auto v : graph.GetVerticesIds()) {
+    d[v] = h[v] = 0;
+    visited[v] = false;
+  }
+
+  std::vector<T> joint_vertices;
+  DFSForJointVertices(d, h, visited, joint_vertices, graph,
+                      graph.GetVerticesIds()[0]);
+
+  return joint_vertices;
+}
