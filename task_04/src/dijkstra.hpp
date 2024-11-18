60c419ce8e6e28fcb4e1c6e59159c571ddc593af
MrWh1teF0x <142975991+MrWh1teF0x@users.noreply.github.com>
2024-11-18 19:31:06 +0000

    add solution for task_04

---
+++ task_04/src/dijkstra.hpp
@@ -0,0 +1,104 @@
+#include <algorithm>
+#include <limits>
+#include <unordered_map>
+
+#include "graph/weighted_graph.hpp"
+
+template <typename T>
+struct MinPath {
+ public:
+  int weight;
+  std::vector<T> vertices;
+
+  void PrintMinPath() {
+    for (int i = 0; i < vertices.size(); i++) {
+      std::cout << vertices[i];
+      if (i != vertices.size() - 1) std::cout << "->";
+    }
+    if (weight != std::numeric_limits<int>::max())
+      std::cout << ": " << weight << '\n';
+    else
+      std::cout << ": inf\n";
+  }
+};
+
+template <typename T>
+void DijkstraStep(WeightedGraph<T>& graph,
+                  std::unordered_map<T, MinPath<T>>& min_paths,
+                  std::unordered_map<T, bool>& visited) {
+  int min_weight = std::numeric_limits<int>::max();
+  std::pair<T, T> edge;
+
+  for (auto [v, is_visited] : visited) {
+    if (is_visited) {
+      int weight_v = min_paths[v].weight;
+      for (auto u : graph.GetAdjVertices(v)) {
+        if (!visited[u]) {
+          int edge_weight = graph.GetEdgeWeight(v, u);
+          if (min_weight > weight_v + edge_weight) {
+            min_weight = weight_v + edge_weight;
+            edge.first = v;
+            edge.second = u;
+          }
+        }
+      }
+    }
+  }
+
+  // Случай, когда какие-то вершины недостижимы
+  if (!edge.first && !edge.second) return;
+
+  if (visited.size() && min_paths.size()) {
+    T v = edge.first, u = edge.second;  // v - родитель, u - ребенок
+    visited[u] = true;
+
+    // Добавляем минимальный найденный путь для вершины
+    for (auto v : min_paths[v].vertices) min_paths[u].vertices.push_back(v);
+    min_paths[u].vertices.push_back(v);
+    min_paths[u].weight = min_weight;
+  }
+
+  for (auto [v, is_visited] : visited)
+    if (!is_visited) DijkstraStep(graph, min_paths, visited);
+}
+
+template <typename T>
+std::vector<MinPath<T>> Dijkstra(const T& vertex, WeightedGraph<T> graph) {
+  if (graph.IsOriented())
+    throw std::invalid_argument("Weighted graph is oriented!");
+
+  for (auto w_edge : graph.GetWeightedEdges())
+    if (w_edge.weight < 0)
+      throw std::invalid_argument(
+          "Weighted graph has edges with negative weights!");
+
+  std::unordered_map<T, MinPath<T>> min_paths;
+  std::unordered_map<T, bool> visited;
+  for (auto v : graph.GetVerticesIds()) {
+    if (v == vertex)
+      visited[v] = true;
+    else {
+      min_paths[v].weight = std::numeric_limits<int>::max();
+      visited[v] = false;
+    }
+  }
+
+  DijkstraStep(graph, min_paths, visited);
+
+  // Удаляем вершину, для которой мы находили кратчайшие пути до других вершин
+  min_paths.erase(vertex);
+
+  std::vector<MinPath<T>> paths;
+  for (auto [v, min_path] : min_paths) {
+    // Добавляем в путь конечную вершину
+    min_path.vertices.push_back(v);
+
+    paths.push_back(min_path);
+  }
+
+  // Переворачиваем массив, чтобы конечными точками в пути являлись первые
+  // добавленные вершины в графе
+  reverse(paths.begin(), paths.end());
+
+  return paths;
+}
