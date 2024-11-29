4e031da8a72087e7c07461d9396bba44eda37991
Dmitry Krivoruchko <144147209+UmbrellaLeaf5@users.noreply.github.com>
2024-11-29 16:25:25 +0300

    primary complete Dijkstra algo

---
+++ task_04/src/dijkstra.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "graph/graph.hpp"
+
+template <AllowedVertType vert_t, AllowedWeightType weight_t>
+std::unordered_map<vert_t, weight_t> Dijkstra(
+    const Graph<vert_t, weight_t>& graph, const vert_t& start) {
+  if (graph.Verts().empty()) return {};
+
+  std::unordered_map<vert_t, weight_t> distances;
+  std::unordered_map<vert_t, bool> visited;
+
+  // инициализация расстояний до бесконечности
+  for (const auto& vert : graph.Verts()) {
+    distances[vert] = std::numeric_limits<weight_t>::infinity();
+    visited[vert] = false;
+  }
+
+  distances[start] = 0;
+
+  for (size_t i = 0; i < graph.VertsAmount(); i++) {
+    vert_t min_vert =
+        graph.Verts()[0];  // находим вершину с минимальным расстоянием
+    weight_t min_distance = std::numeric_limits<weight_t>::infinity();
+
+    for (const auto& node : graph.Verts())
+      if (!visited[node] && distances[node] < min_distance) {
+        min_vert = node;
+        min_distance = distances[node];
+      }
+
+    if (min_distance == std::numeric_limits<weight_t>::infinity())
+      break;  // все достижимые вершины обработаны
+
+    visited[min_vert] = true;
+
+    // релаксация ребер
+    const auto neighbors = graph.GetAdjList()[min_vert];
+    for (const auto& vert : neighbors) {
+      auto weight = graph.GetWeightOfEdge({min_vert, vert});
+
+      if (distances[min_vert] + weight < distances[vert])
+        distances[vert] = distances[min_vert] + weight;
+    }
+  }
+
+  return distances;
+}
