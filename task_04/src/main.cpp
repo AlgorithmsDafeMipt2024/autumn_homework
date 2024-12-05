dec3df32afb2f8c9f0365f23f7b96d3b58b280e1
Arkadiy <69438372+EchoPr@users.noreply.github.com>
2024-12-05 12:10:35 +0000

    feat(tests): Tests added for Dijkstra

--- task_04/src/main.cpp
+++ task_04/src/main.cpp
@@ -1,36 +1,11 @@
 #include <climits>
 #include <iostream>
-#include <queue>
-#include <utility>
-#include <vector>
 
 #include "graph.hpp"
+#include "util.hpp"
 
 using namespace algo;
 using namespace std;
-
-void dijkstra(int v, int from, vector<int>& vis, vector<int>& dst,
-              const AdjacencyList adj) {
-  priority_queue<pair<int, int>, vector<pair<int, int>>,
-                 greater<pair<int, int>>>
-      q;
-
-  q.push({0, v});
-  while (!q.empty()) {
-    auto [cur_d, v] = q.top();
-    q.pop();
-
-    if (cur_d > dst[v]) continue;
-
-    for (auto [u, w] : adj[v]) {
-      if (dst[u] > dst[v] + w) {
-        dst[u] = dst[v] + w;
-
-        q.push({dst[u], u});
-      }
-    }
-  }
-}
 
 int main() {
   int vertexes_num, edges_num;
@@ -43,20 +18,16 @@ int main() {
 
     cin >> a >> b >> w;
 
-    graph.addEdge(a, b, w);
+    graph.AddEdge(a, b, w);
   }
-
-  vector<int> vis(vertexes_num);
-  vector<int> dst(vertexes_num, INT_MAX);
 
   int start;
   cin >> start;
 
   start--;
-  dst[start] = 0;
-  dijkstra(start, -1, vis, dst, graph.getAdjList());
+  auto ways = FindWays(graph, start);
 
-  for (int i = 0; i < dst.size(); i++) {
-    cout << i + 1 << ": " << dst[i] << endl;
+  for (int i = 0; i < ways.size(); i++) {
+    cout << i + 1 << ": " << ways[i] << endl;
   }
 }
