7fe243b27851735d909afc9eb3301a57f02f2fdb
Sukhanov George <sukhanov.ge@phystech.edu>
2025-01-09 22:22:16 +0000

    fix

--- lib/src/graph.hpp
+++ lib/src/graph.hpp
@@ -3,8 +3,6 @@
 #include <cmath>
 #include <limits>
 #include <list>
-#include <memory>
-#include <queue>
 #include <stdexcept>
 #include <vector>
 
@@ -22,7 +20,7 @@ class Graph {
 
  public:
   Graph(size_t number) {
-    if (number <= 0) throw std::logic_error("number must be positive!");
+    if (number == 0) throw std::logic_error("number must be positive!");
     vertices_number_ = number;
     adjacency_list_.resize(vertices_number_ + 1);
   }
@@ -64,118 +62,18 @@ class WeightedGraph {
   vector<vector<int>> table_;
   size_t vertices_number_;
 
-  void CheckNegativeCycle(const vector<int>& distances) {
-    for (size_t u = 0; u < vertices_number_; ++u) {
-      for (size_t v = 0; v < vertices_number_; ++v) {
-        if (table_[u][v] != INF && distances[u] != INF) {
-          if (distances[u] + table_[u][v] < distances[v]) {
-            throw std::logic_error("Graph contains a negative weight cycle");
-          }
-        }
-      }
-    }
-  }
+  void CheckNegativeCycle(const vector<int>& distances);
 
-  bool RelaxEdges(vector<int>& distances) {
-    bool updated = false;
-    for (size_t u = 0; u < vertices_number_; ++u) {
-      for (size_t v = 0; v < vertices_number_; ++v) {
-        if (table_[u][v] != INF && distances[u] != INF) {
-          if (distances[u] + table_[u][v] < distances[v]) {
-            distances[v] = distances[u] + table_[u][v];
-            updated = true;
-          }
-        }
-      }
-    }
-    return updated;
-  }
+  bool RelaxEdges(vector<int>& distances);
 
-  vector<int> BellmanFord(size_t start) {
-    vector<int> distances(vertices_number_, INF);
-    distances[start] = 0;
-
-    for (size_t i = 0; i < vertices_number_ - 1; ++i) {
-      RelaxEdges(distances);
-    }
-
-    CheckNegativeCycle(distances);
-    return distances;
-  }
+  vector<int> BellmanFord(size_t start);
 
  public:
-  WeightedGraph(vector<vector<int>>&& table)
-      : table_(std::move(table)), vertices_number_(table_.size()) {
-    if (table_.empty())
-      throw std::logic_error(
-          "the graph was not created because the input table is empty");
-    for (size_t i = 0; i < table_.size(); i++)
-      if (table_[i].size() != table_.size())
-        throw std::logic_error(
-            "the graph was not created because the input table is incorrect");
-  }
+  WeightedGraph(vector<vector<int>>&& table);
 
-  vector<int> Dijkstra(size_t start) {
-    vector<int> distances(vertices_number_, INF);
-    distances[start] = 0;
+  vector<int> Dijkstra(size_t start);
 
-    std::priority_queue<pair<int, int>, vector<pair<int, int>>, std::greater<>>
-        pq;
-    pq.push({0, start});
-
-    while (!pq.empty()) {
-      const int u = pq.top().second;
-      const int dist_u = pq.top().first;
-      pq.pop();
-
-      if (dist_u > distances[u]) continue;
-
-      for (size_t v = 0; v < vertices_number_; ++v) {
-        if (table_[u][v] != INF && distances[u] != INF) {
-          const int new_dist = distances[u] + table_[u][v];
-          if (new_dist < distances[v]) {
-            distances[v] = new_dist;
-            pq.push({new_dist, v});
-          }
-        }
-      }
-    }
-    return distances;
-  }
-
-  vector<vector<int>> Johnson() {
-    vector<int> h(vertices_number_ + 1, 0);
-
-    table_.push_back(vector<int>(vertices_number_, 0));
-    try {
-      h = BellmanFord(vertices_number_);
-    } catch (const std::logic_error&) {
-      throw std::logic_error(
-          "Graph contains a negative weight cycle, Johnson's algorithm cannot "
-          "be applied");
-    }
-    table_.pop_back();
-
-    for (size_t u = 0; u < vertices_number_; ++u) {
-      for (size_t v = 0; v < vertices_number_; ++v) {
-        if (table_[u][v] != INF) {
-          table_[u][v] += h[u] - h[v];
-        }
-      }
-    }
-
-    vector<vector<int>> distances(vertices_number_);
-    for (size_t u = 0; u < vertices_number_; ++u) {
-      distances[u] = Dijkstra(u);
-      for (size_t v = 0; v < vertices_number_; ++v) {
-        if (distances[u][v] != INF) {
-          distances[u][v] += h[v] - h[u];
-        }
-      }
-    }
-
-    return distances;
-  }
+  vector<vector<int>> Johnson();
 };
 
 template <typename T>
