31562c991ab756cdec7a77ec9d60b3911799820f
Dmitry Krivoruchko <144147209+UmbrellaLeaf5@users.noreply.github.com>
2025-01-11 04:12:50 +0300

    update README.md in task_03 and task_02

--- task_02/src/main.cpp
+++ task_02/src/main.cpp
@@ -4,19 +4,57 @@
 #include "find_cut_vertices.hpp"
 
 int main() {
-  Graph<int, long> g;
-  g.AddEdge({0, 1});
-  g.AddEdge({0, 2});
-  g.AddEdge({1, 2});
-  g.AddEdge({3, 2});
+  // Пример использования:
+  // 1. Задаем граф
+  Graph<int, int> network_graph;
 
-  g.MakeUndirected();
+  // Добавляем вершины
+  network_graph.AddVert(0);
+  network_graph.AddVert(1);
+  network_graph.AddVert(2);
+  network_graph.AddVert(3);
+  network_graph.AddVert(4);
+  network_graph.AddVert(5);
 
-  std::cout << g << std::endl;
-  std::cout << "Bridges: " << std::endl;
-  std::cout << FindBridges(g) << std::endl;
-  std::cout << "CutVertices: " << std::endl;
-  std::cout << FindCutVertices(g) << std::endl;
+  // Добавляем ребра
+  network_graph.AddEdge({0, 1});
+  network_graph.AddEdge({1, 2});
+  network_graph.AddEdge({2, 0});
+  network_graph.AddEdge({1, 3});
+  network_graph.AddEdge({3, 4});
+  network_graph.AddEdge({4, 5});
+  network_graph.AddEdge({3, 5});
+
+  // Пример графа с 2 компонентами связности
+  //    network_graph.AddVert(6);
+  //    network_graph.AddVert(7);
+  //    network_graph.AddEdge({6,7});
+
+  // 2. Находим мосты
+  std::vector<std::pair<int, int>> bridges = FindBridges(network_graph);
+
+  // 3. Находим точки сочленения
+  std::vector<int> cut_vertices = FindCutVertices(network_graph);
+
+  // 4. Выводим результаты
+  std::cout << "Bridges (wires that, if damaged, will disconnect the network):"
+            << std::endl;
+
+  if (bridges.empty())
+    std::cout << "No bridges were found in the network." << std::endl;
+  else
+    for (const auto& bridge : bridges)
+      std::cout << "(" << bridge.first << ", " << bridge.second << ")"
+                << std::endl;
+
+  std::cout << "\nCut Vertices (routers that, if out of order, will disconnect "
+               "the network):"
+            << std::endl;
+  if (cut_vertices.empty())
+    std::cout << "No cut vertices were found in the network." << std::endl;
+  else
+    for (const auto& cut_vertice : cut_vertices)
+      std::cout << cut_vertice << std::endl;
 
   return 0;
 }
