d208a9d2035f80419ab37c5041f758645353318f
Lida <105884035+Salnikova-Lida@users.noreply.github.com>
2025-01-15 12:49:04 +0000

    task 4

--- task_04/src/main.cpp
+++ task_04/src/main.cpp
@@ -1,3 +1,38 @@
 #include <iostream>
+#include <limits>
+#include <map>
+#include <vector>
 
-int main() { return 0; }
+std::vector<double> Dijkstra(std::vector<std::map<int, double>> data, int source) {
+    std::vector<double> ans(data.size(), std::numeric_limits<double>::infinity());
+    std::vector<bool> counted(data.size(), false);
+    ans[source] = 0;
+    counted[source] = true;
+    int pending = source;
+    double min_lenght;
+    for (int i = 0; i < data.size() - 1; ++i) {
+        for (auto neighbour : data[pending]) {
+            if (ans[neighbour.first] > neighbour.second + ans[pending]) {
+                ans[neighbour.first] = neighbour.second + ans[pending];
+            }
+        }
+        min_lenght = std::numeric_limits<double>::infinity();
+        for (int j = 0; j < data.size(); j++) {
+            if ((ans[j] < min_lenght)&& !counted[j]) {
+                min_lenght = ans[j];
+                pending = j;
+            }
+        }
+        counted[pending] = true;
+    }
+    return ans;
+}
+
+int main() { 
+    std::vector<std::map<int, double>> data = {{{1, 1}, {2, 8}, {3, 5}, {4, 6}, {5, 6}}, {{2, 1}, {3, 11}, {4, 1}, {5, 8}}, {{3, 2}}, {{4, 2}}, {{5, 4}}, {{2, 1}}};
+    std::vector<double> ans = Dijkstra(data, 0);
+    for (auto elem : ans){
+        std::cout << elem << std::endl;
+    }
+    return 0; 
+}
--- task_04/src/test.cpp
+++ task_04/src/test.cpp
@@ -1,6 +1,27 @@
 
 #include <gtest/gtest.h>
+#include "main.cpp"
 
-TEST(TopologySort, Simple) {
-  ASSERT_EQ(1, 1);  // Stack []
+TEST(DijkstraTest, Simple) {
+  std::vector<std::map<int, double>> data = {{{1, 8}, {2, 2}}, {{3, 4}}, {{1, 3}}, {}};
+  std::vector<double> ans = {0, 5, 2, 9};
+  ASSERT_EQ(Dijkstra(data, 0), ans); 
+}
+
+TEST(DijkstraTest, Pyramid) {
+  std::vector<std::map<int, double>> data = {{{1, 1}, {2, 8}, {3, 5}, {4, 6}, {5, 6}}, {{2, 1}, {3, 11}, {4, 1}, {5, 8}}, {{3, 2}}, {{4, 2}}, {{5, 4}}, {{2, 1}}};
+  std::vector<double> ans = {0, 1, 2, 4, 2, 6};
+  ASSERT_EQ(Dijkstra(data, 0), ans); 
+}
+TEST(DijkstraTest, Sophisticated) {
+    std::vector<std::map<int, double>> data = {{{1, 1}, {2, 8}, {3, 3}, {4, 5}, {5, 9}}, {{2, 2}, {3, 3}, {4, 2}}, {{3, 3}, {5, 4}}, {{4, 4}, {5, 6}, {6, 15}}, {{6, 10}}, {{6, 4}}, {}};
+  std::vector<double> ans = {0, 1, 3, 3, 3, 7, 11};
+  ASSERT_EQ(Dijkstra(data, 0), ans);
+}
+
+
+TEST(DijkstraTest, SimpleCycle) {
+    std::vector<std::map<int, double>> data = {{{1, 9}, {2, 8}, {3, 3}}, {{4, 1}}, {{1, 2}, {3, 1}}, {{4, 2}}, {{2, 1}}};
+  std::vector<double> ans = {0, 8, 6, 3, 5};
+  ASSERT_EQ(Dijkstra(data, 0), ans);
 }
