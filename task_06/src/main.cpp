493682c76926aeedb10680c3068966687005f3cc
Vanek <123vanichka321@gmail.com>
2025-01-15 17:39:58 +0300

    add task 6

--- task_06/src/main.cpp
+++ task_06/src/main.cpp
@@ -1,3 +1,47 @@
 #include <iostream>
 
-int main() { return 0; }
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val) {
+        data = val;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+Node* lca(Node* root, int n1, int n2) {
+
+    if (root == nullptr)
+        return nullptr;
+    if (root->data > n1 && root->data > n2)
+        return lca(root->left, n1, n2);
+    if (root->data < n1 && root->data < n2)
+        return lca(root->right, n1, n2);
+    return root;
+}
+
+int main() {
+    Node* root = new Node(20);
+    root->left = new Node(8);
+    root->right = new Node(22);
+    root->left->left = new Node(4);
+    root->left->right = new Node(12);
+    root->left->right->left = new Node(10);
+    root->left->right->right = new Node(14);
+
+    int n1 = 10, n2 = 14;
+    Node* t = lca(root, n1, n2);
+    std::cout << n1 << " < " << t->data << " < " << n2 << std::endl;
+
+    n1 = 14, n2 = 8;
+    t = lca(root, n1, n2);
+    std::cout << n1 << " < " << t->data << " < " << n2 << std::endl;
+
+    n1 = 10, n2 = 22;
+    t = lca(root, n1, n2);
+    std::cout<< n1 << " < " << t->data << " < " << n2 << std::endl;
+
+    return 0;
+}
