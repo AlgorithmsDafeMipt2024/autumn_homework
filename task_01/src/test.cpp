ce24e9a690e96f479ba4061c6b2715cab129d361
Lynech <144128009+Lynech@users.noreply.github.com>
2025-01-15 01:29:43 +0000

    remaning in tests (text editors're not working for some reason)

--- task_01/src/test.cpp
+++ task_01/src/test.cpp
@@ -3,54 +3,54 @@
 #include "graph.tpp"
 
 TEST(TopologicalSort, test1) {
-  Graph<int> g(1);
+  Graph<int> a(1);
 
-  EXPECT_EQ(g.TopologicalSort(), std::list<int>({0}));
+  EXPECT_EQ(a.TopologicalSort(), std::list<int>({0}));
 }
 
 TEST(TopologicalSort, test2) {
-  Graph<int> g(2);
+  Graph<int> a(2);
 
-  g.AddEdge(0, 1);
+  a.AddEdge(0, 1);
 
-  EXPECT_EQ(g.TopologicalSort(), std::list<int>({0, 1}));
+  EXPECT_EQ(a.TopologicalSort(), std::list<int>({0, 1}));
 }
 
 TEST(TopologicalSort, test3) {
-  Graph<int> g(3);
+  Graph<int> a(3);
 
-  g.AddEdge(0, 1);
-  g.AddEdge(1, 2);
+  a.AddEdge(0, 1);
+  a.AddEdge(1, 2);
 
-  EXPECT_EQ(g.TopologicalSort(), std::list<int>({0, 1, 2}));
+  EXPECT_EQ(a.TopologicalSort(), std::list<int>({0, 1, 2}));
 }
 
 TEST(TopologicalSort, test4) {
-  Graph<int> g(6);
+  Graph<int> a(6);
 
-  g.AddEdge(0, 1);
-  g.AddEdge(0, 3);
-  g.AddEdge(1, 2);
-  g.AddEdge(3, 1);
-  g.AddEdge(3, 5);
-  g.AddEdge(3, 4);
-  g.AddEdge(4, 5);
+  a.AddEdge(0, 1);
+  a.AddEdge(0, 3);
+  a.AddEdge(1, 2);
+  a.AddEdge(3, 1);
+  a.AddEdge(3, 5);
+  a.AddEdge(3, 4);
+  a.AddEdge(4, 5);
 
-  EXPECT_EQ(g.TopologicalSort(), std::list<int>({0, 3, 4, 5, 1, 2}));
+  EXPECT_EQ(a.TopologicalSort(), std::list<int>({0, 3, 4, 5, 1, 2}));
 }
 
 TEST(TopologicalSort, test5) {
-  Graph<int> g(7);
+  Graph<int> a(7);
 
-  g.AddEdge(0, 1);
-  g.AddEdge(0, 2);
-  g.AddEdge(1, 2);
-  g.AddEdge(1, 5);
-  g.AddEdge(2, 3);
-  g.AddEdge(5, 3);
-  g.AddEdge(5, 4);
-  g.AddEdge(6, 1);
-  g.AddEdge(6, 5);
+  a.AddEdge(0, 1);
+  a.AddEdge(0, 2);
+  a.AddEdge(1, 2);
+  a.AddEdge(1, 5);
+  a.AddEdge(2, 3);
+  a.AddEdge(5, 3);
+  a.AddEdge(5, 4);
+  a.AddEdge(6, 1);
+  a.AddEdge(6, 5);
 
-  EXPECT_EQ(g.TopologicalSort(), std::list<int>({6, 0, 1, 5, 4, 2, 3}));
+  EXPECT_EQ(a.TopologicalSort(), std::list<int>({6, 0, 1, 5, 4, 2, 3}));
 }
