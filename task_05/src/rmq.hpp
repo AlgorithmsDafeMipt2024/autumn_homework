261775d3e65fc1d0de6c1ff6846d885d40e0e528
Lida <105884035+Salnikova-Lida@users.noreply.github.com>
2025-01-16 20:07:00 +0000

    task 5 with test

---
+++ task_05/src/rmq.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <vector>
+#include <cmath>
+#include <math.h>
+#include <algorithm>
+#include <limits>
+std::vector<std::vector<double>> sparse(std::vector<double> &data) {
+  std::vector<std::vector<double>> sparse_table(log2(data.size()) + 1, std::vector<double>(data.size(),std::numeric_limits<double>::infinity()));
+  auto start = data.begin();
+  auto end = data.end();
+  int height;
+  for (int i = 0; i < sparse_table.size(); ++i) {
+    for (int j = data.size() - 1; j >= 0; --j) {
+      start = data.begin() + j + 1;
+      height = std::pow(2, i);
+      end = start - height;
+      if (end >= data.begin()) {
+        sparse_table[i][j] = *std::min_element(end, start);
+      }
+    }
+  }
+  return sparse_table;
+}
+
+double inf(std::vector<std::vector<double>>& sparse_table, int u, int v) {
+  int const width = log2(v - u + 1);
+  if (std::pow(2, width) == (v - u + 1)) {
+    return sparse_table[width][v];
+  }
+  return std::min( sparse_table[width][u + std::pow(2, width) - 1], sparse_table[width][v]);
+}
--- task_05/src/test.cpp
+++ task_05/src/test.cpp
@@ -1,6 +1,37 @@
 
 #include <gtest/gtest.h>
+#include "rmq.hpp"
 
-TEST(TopologySort, Simple) {
-  ASSERT_EQ(1, 1);  // Stack []
+TEST(RMQ, Simple) {
+  std::vector<double> data = {1, 4, 7, 9, 4, 4, 3, 2, 7, 9, 5};
+  std::vector<std::vector<double>> table = sparse(data);
+  double answer = inf(table, 1, 5);
+  double correct = 4;
+  ASSERT_EQ(answer, correct); 
 }
+
+TEST(RMQ, Simple2) {
+  std::vector<double> data = {1, 4, 7, 9, 4, 4, 3, 2, 7, 9, 5};
+  std::vector<std::vector<double>> table = sparse(data);
+  double answer = inf(table, 3, 8);
+  double correct = 2;
+  ASSERT_EQ(answer, correct); 
+}
+
+TEST(RMQ, Whole) {
+  std::vector<double> data = {1, 4, 7, 9, 4, 4, 3, 2, 7, 9, 5};
+  std::vector<std::vector<double>> table = sparse(data);
+  double answer = inf(table, 0, 10);
+  double correct = 1;
+  ASSERT_EQ(answer, correct);
+}
+
+TEST(RMQ, Big) {
+  std::vector<double> data = {1, 2, 3, 1, 4, 8, 3, 6, 8, 3, 7, 8, 9, 5, 3, 4, 8, 34, 8, 6, 12, 8, 4, 8, 5, 9, 7, 8, 5, 7, 9, 7, 8, 4, 8, 12, 5, 7, 9, 6, 8, 14, 13, 12, 8, 7, 6, 9, 8, 3, 6, 8, 6, 11, 10, 9, 8, 11, 2};
+  std::vector<std::vector<double>> table = sparse(data);
+  double answer = inf(table, 15, 54);
+  double correct = 3;
+  ASSERT_EQ(answer, correct);
+}
+
+
