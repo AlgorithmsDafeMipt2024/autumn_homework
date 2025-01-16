c42ad030d3e646e74e7a70951f30ab0ed87f5cf1
Alexander Kosarigin <123541292+LightAboveFighter@users.noreply.github.com>
2025-01-16 14:39:10 +0000

    format

--- task_02/src/find_bridges_cut_verts.hpp
+++ task_02/src/find_bridges_cut_verts.hpp
@@ -2,8 +2,8 @@
 #define FIND_BRIDGES_CUT_VERTS_H_
 
 #include <vector>
-#include "../../lib/src/graph.h"
 
+#include "../../lib/src/graph.h"
 
 bool dfs_path_to_vert_exist(Graph& g, std::vector<int>& path_visited,
                             std::pair<int, int>& deleted_edge, int vert,
@@ -25,8 +25,7 @@ std::vector<std::pair<int, int>> get_bridges_taryan(Graph& g);
 
 void dfs_taryan_verts(Graph& g, std::vector<bool>& visited,
                       std::vector<int>& tin, std::vector<int>& fup,
-                      std::vector<int>& cut_verts, int time, int v,
-                      int p);
+                      std::vector<int>& cut_verts, int time, int v, int p);
 
 std::vector<int> get_cut_verts_taryan(Graph& g);
 
--- task_02/src/test.cpp
+++ task_02/src/test.cpp
@@ -38,8 +38,8 @@ TEST(get_bridges, Test_1) {
   Graph g(9, {{0, 1}, {0, 2}, {1, 2}, {2, 3}, {3, 4}, {5, 6}, {1, 6}, {2, 6},
               {3, 7}, {7, 8}, {4, 8}, {1, 0}, {2, 0}, {2, 1}, {3, 2}, {4, 3},
               {6, 5}, {6, 1}, {6, 2}, {7, 3}, {8, 7}, {8, 4}});
-  std::vector<std::pair<int,int>> bridges = get_bridges(g);
-  std::initializer_list<std::pair<int,int>> true_bridges = {{2, 3}, {5, 6}};
+  std::vector<std::pair<int, int>> bridges = get_bridges(g);
+  std::initializer_list<std::pair<int, int>> true_bridges = {{2, 3}, {5, 6}};
 
   ASSERT_EQ(vector_pair_equal(bridges, true_bridges), true);
 }
@@ -48,8 +48,8 @@ TEST(get_bridges_taryan, Test_1) {
   Graph g(9, {{0, 1}, {0, 2}, {1, 2}, {2, 3}, {3, 4}, {5, 6}, {1, 6}, {2, 6},
               {3, 7}, {7, 8}, {4, 8}, {1, 0}, {2, 0}, {2, 1}, {3, 2}, {4, 3},
               {6, 5}, {6, 1}, {6, 2}, {7, 3}, {8, 7}, {8, 4}});
-  std::vector<std::pair<int,int>> bridges = get_bridges_taryan(g);
-  std::initializer_list<std::pair<int,int>> true_bridges = {{2, 3}, {5, 6}};
+  std::vector<std::pair<int, int>> bridges = get_bridges_taryan(g);
+  std::initializer_list<std::pair<int, int>> true_bridges = {{2, 3}, {5, 6}};
 
   ASSERT_EQ(vector_pair_equal(bridges, true_bridges), true);
 }
@@ -66,16 +66,16 @@ TEST(get_cut_verts_taryan, Test_1) {
 
 TEST(get_bridges, Test_2) {
   Graph g(3, {{0, 1}, {1, 0}, {0, 2}, {2, 0}, {1, 2}, {2, 1}});
-  std::vector<std::pair<int,int>> bridges = get_bridges(g);
-  std::initializer_list<std::pair<int,int>> true_bridges = {};
+  std::vector<std::pair<int, int>> bridges = get_bridges(g);
+  std::initializer_list<std::pair<int, int>> true_bridges = {};
 
   ASSERT_EQ(vector_pair_equal(bridges, true_bridges), true);
 }
 
 TEST(get_bridges_taryan, Test_2) {
   Graph g(3, {{0, 1}, {1, 0}, {0, 2}, {2, 0}, {1, 2}, {2, 1}});
-  std::vector<std::pair<int,int>> bridges = get_bridges_taryan(g);
-  std::initializer_list<std::pair<int,int>> true_bridges = {};
+  std::vector<std::pair<int, int>> bridges = get_bridges_taryan(g);
+  std::initializer_list<std::pair<int, int>> true_bridges = {};
 
   ASSERT_EQ(vector_pair_equal(bridges, true_bridges), true);
 }
@@ -90,16 +90,16 @@ TEST(get_cut_verts_taryan, Test_2) {
 
 TEST(get_bridges, Test_3) {
   Graph g(4, {{0, 1}, {1, 0}, {0, 2}, {2, 0}, {1, 2}, {2, 1}, {0, 3}, {3, 0}});
-  std::vector<std::pair<int,int>> bridges = get_bridges(g);
-  std::initializer_list<std::pair<int,int>> true_bridges = {{0, 3}};
+  std::vector<std::pair<int, int>> bridges = get_bridges(g);
+  std::initializer_list<std::pair<int, int>> true_bridges = {{0, 3}};
 
   ASSERT_EQ(vector_pair_equal(bridges, true_bridges), true);
 }
 
 TEST(get_bridges_taryan, Test_3) {
   Graph g(4, {{0, 1}, {1, 0}, {0, 2}, {2, 0}, {1, 2}, {2, 1}, {0, 3}, {3, 0}});
-  std::vector<std::pair<int,int>> bridges = get_bridges_taryan(g);
-  std::initializer_list<std::pair<int,int>> true_bridges = {{0, 3}};
+  std::vector<std::pair<int, int>> bridges = get_bridges_taryan(g);
+  std::initializer_list<std::pair<int, int>> true_bridges = {{0, 3}};
 
   ASSERT_EQ(vector_pair_equal(bridges, true_bridges), true);
 }
