c4fcb299ce8e8a28cd4077a39780d416fc7778d7
Ivan Gousev <vanyagousev@gmail.com>
2025-01-16 13:38:45 +0300

    6 tasks

--- task_06/src/main.cpp
+++ task_06/src/main.cpp
@@ -1,3 +1,12 @@
 #include <iostream>
+#include <vector>
+#include "lca.h"
 
-int main() { return 0; }
+
+int main() {
+  std::vector<std::vector<int>> g = {{0,1,2},{3,4},{},{},{}};
+    std::cout<<"as";
+  auto a = lca(g, {{3,4},{3,5}});
+  std::cout<<a[0]<<" "<<a[1]; 
+  return 0;
+}
--- task_06/src/test.cpp
+++ task_06/src/test.cpp
@@ -1,6 +1,15 @@
-
 #include <gtest/gtest.h>
+#include "lca.h"
 
-TEST(TopologySort, Simple) {
-  ASSERT_EQ(1, 1);  // Stack []
+TEST(TEST, Simple) {
+  std::vector<std::vector<int>> g = {{1,2},{3,4},{},{},{}};
+    std::cout<<"as";
+  auto a = lca(g, {{3,4},{3,5}});
+  ASSERT_TRUE(a[0]=1&&a[1]==0);
+}
+TEST(TEST, Simple1) {
+  std::vector<std::vector<int>> g = {{1},{2},{3},{4},{}};
+    std::cout<<"as";
+  auto a = lca(g, {{3,4},{1,4},{4,1}});
+  ASSERT_TRUE(a[0]=3&&a[1]==1&&a[2]==1)<<a[1];
 }
