6617e58a8de77192891b7280015d170ef6eac3ae
Mox <mox.dev@yandex.ru>
2025-01-17 00:55:27 +0300

    Put dijkstra inside of the task 03

--- task_03/src/bell_john.hpp
+++ task_03/src/bell_john.hpp
@@ -1,7 +1,15 @@
-#include "dijkstra.hpp"
+#include <limits>
 #include <vector>
-#include <limits>
-#include <limits>
+#include <queue>
+#include <map>
+#include "util.hpp"
+
+struct FirstGreater {
+    bool operator()(std::pair<double, int> const& lhs,
+                    std::pair<double, int> const& rhs) const;
+};
+
+std::vector<double> dijkstra(OrientedWeightedGraph graph, int from_key);
 
 std::vector<double> bellman(OrientedWeightedGraph graph, int from_key, int iterations = std::numeric_limits<int>::infinity());
 
