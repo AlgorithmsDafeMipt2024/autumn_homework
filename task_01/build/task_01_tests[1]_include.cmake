d9e3c8593ee5fbcd673809fef92a750bc07e52ba
Ivan Gousev <vanyagousev@gmail.com>
2025-01-16 14:15:49 +0300

    clean

--- task_01/build/task_01_tests[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/home/iven/Documents/mipt/autumn_homework/task_01/build/task_01_tests[1]_tests.cmake")
-  include("/home/iven/Documents/mipt/autumn_homework/task_01/build/task_01_tests[1]_tests.cmake")
-else()
-  add_test(task_01_tests_NOT_BUILT task_01_tests_NOT_BUILT)
-endif()
--- task_01/build/task_01_tests[1]_tests.cmake
+++
@@ -1,3 +0,0 @@
-add_test([=[Test.Simple]=]  /home/iven/Documents/mipt/autumn_homework/task_01/build/task_01_tests [==[--gtest_filter=Test.Simple]==] --gtest_also_run_disabled_tests)
-set_tests_properties([=[Test.Simple]=]  PROPERTIES WORKING_DIRECTORY /home/iven/Documents/mipt/autumn_homework/task_01/build SKIP_REGULAR_EXPRESSION [==[\[  SKIPPED \]]==])
-set(  task_01_tests_TESTS Test.Simple)
